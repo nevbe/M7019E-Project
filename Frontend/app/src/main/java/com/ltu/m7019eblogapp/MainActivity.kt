111b03e8575c96b2569b92b152d8a5b05a6b6564
Elliot <ellioth70@gmail.com>
2023-04-21 18:09:34 +0200

    Profile icon now properly displays

--- Frontend/app/src/main/java/com/ltu/m7019eblogapp/MainActivity.kt
+++ Frontend/app/src/main/java/com/ltu/m7019eblogapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ltu.m7019eblogapp
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -19,30 +20,29 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(findViewById(R.id.topAppBar))
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         //TODO: Reformat this for the used top bar (code is for native app bar)
-        /*
-
-        setSupportActionBar(findViewById(R.id.topAppBar))
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-         val appBarConfiguration = AppBarConfiguration(
+        val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-         ))
+            )
+        )
 
-          setupActionBarWithNavController(navController, appBarConfiguration)
-
-         */
-
-
-
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_header_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
