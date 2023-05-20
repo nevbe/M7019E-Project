dc2016bbff0a5c76f3bd551918c997836e2af2b8
Elliot <ellioth70@gmail.com>
2023-05-21 00:09:11 +0200

    Create post checks kind of work heh

--- Frontend/app/src/main/java/com/ltu/m7019eblogapp/ui/createpost/CreatePostFragment.kt
+++ Frontend/app/src/main/java/com/ltu/m7019eblogapp/ui/createpost/CreatePostFragment.kt
@@ -67,124 +67,28 @@ class CreatePostFragment : Fragment() {
         _binding = FragmentCreatePostBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // =========================================================================================
-        // ---------------------------- CATEGORY AUTOFILL ------------------------------------------
-        // =========================================================================================
-
-        val initialCategories : MutableList<Category> = mutableListOf()
-        lifecycleScope.launch {
-            initialCategories.addAll(_container.blogRepository.getCategories(1))
+        binding.titleCreate.editText?.addTextChangedListener {
+            if(it.toString().trim().isNotBlank()){
+                binding.titleCreate.error = null
+            }
         }
-
-        val categoryAdapter = ArrayAdapter(requireContext(), R.layout.fragment_create_post_list_item, initialCategories)
-
-        val categorySelectView = binding.categoryCreate.editText as? AutoCompleteTextView
-        categorySelectView?.setAdapter(categoryAdapter)
-
-        categorySelectView?.setOnItemClickListener { parent, _, position, _ ->
-            selectedCategory = parent.getItemAtPosition(position) as Category
-            println("USER SELECTED ${selectedCategory!!.name} AS CATEGORY")
-            // Handle the selected category
+        binding.contentCreate.editText?.addTextChangedListener {
+            if(it.toString().trim().isNotBlank()){
+                binding.contentCreate.error = null
+            }
         }
-
-        categorySelectView?.addTextChangedListener {
-            val query = it.toString().trim()
-            if (query.isNotBlank()) {
-                // Fetch categories based on the search query
-                lifecycleScope.launch {
-                    try {
-                        val searchObject = CategorySearchBody(Category(name=query))
-                        val categories = _container.blogRepository.searchCategory(searchObject)
-                        // TODO: We could clear here for scalability, but this is a mini-project :)
-                        categoryAdapter.addAll(categories)
-                        categoryAdapter.notifyDataSetChanged()
-
-                    } catch (e: Exception) {
-                        // Handle API request failure
-                        println(e.message.toString())
-                    }
-                }
+        binding.mediaCreate.editText?.addTextChangedListener {
+            if(it.toString().trim().isNotBlank()){
+                binding.mediaCreate.error = null
             }
         }
 
-        // ========================================================================================
-        // -------------------------------- TAG AUTOFILL ------------------------------------------
-        // ========================================================================================
+        initCategoryAutoFill()
 
-        val initialTags : MutableList<Tag> = mutableListOf()
-        lifecycleScope.launch {
-            initialTags.addAll(_container.blogRepository.getTags(1))
-        }
-
-        val tagAdapter = ArrayAdapter(requireContext(), R.layout.fragment_create_post_list_item, initialTags)
-
-        val tagSelectView = (binding.tagCreate.editText as? AutoCompleteTextView)
-        tagSelectView?.setAdapter(tagAdapter)
-
-        tagSelectView?.setOnItemClickListener { parent, _, position, _ ->
-            val selectedTag = parent.getItemAtPosition(position) as Tag
-            if(selectedTags[selectedTag.name] != null){
-                val parentView = parent.parent as? TextInputLayout
-                parentView?.error = "Tag already selected"
-            } else {
-                addChip(selectedTag)
-            }
-            tagSelectView.setText("")
-            println("Selected tags: ${selectedTags.keys.toList()}")
-        }
-
-        tagSelectView?.addTextChangedListener {
-            val query = it.toString().trim()
-
-            if (query.isNotBlank() && query != "") {
-                // Fetch categories based on the search query
-                lifecycleScope.launch {
-                    try {
-                        val searchObject = TagSearchBody(Tag(name=query))
-                        val tags = _container.blogRepository.searchTag(searchObject)
-                        // TODO: We could clear here for scalability, but this is a mini-project :)
-                        tagAdapter.clear()
-                        tagAdapter.addAll(tags)
-                        tagAdapter.notifyDataSetChanged()
-
-                    } catch (e: Exception) {
-                        // Handle API request failure
-                        println(e.message.toString())
-                    }
-                }
-            }
-        }
-
+        initTagAutoFill()
 
         val submitBtn = binding.submitCreate
-        submitBtn.setOnClickListener {
-            val title = binding.titleCreate.editText?.text.toString()
-            val content = binding.contentCreate.editText?.text.toString()
-            val media = binding.mediaCreate.editText?.text.toString()
-
-            val tagIDs : MutableList<String> = mutableListOf()
-
-            for(tag in selectedTags){
-                tagIDs.add(tag.value)
-            }
-
-            if(title != "" && content != "" && media != "" && tagIDs.isNotEmpty()) {
-                val post = SubmittablePost(
-                    title, content, selectedCategory!!.id, tagIDs,media
-                )
-
-                println("POST SUBMITTED!")
-                println(post)
-
-                lifecycleScope.launch {
-                    _container.blogRepository.createPost(userSession.accessToken!!,post)
-                }
-            } else {
-                TODO("ERROR: Form not complete!")
-            }
-
-        }
-
+        submitBtn.setOnClickListener { submitPost() }
 
         return root
     }
@@ -205,6 +109,158 @@ class CreatePostFragment : Fragment() {
             setOnCloseIconClickListener{
                 selectedTags.remove((it as Chip).text)
                 (it.parent as ChipGroup).removeView(it)
+            }
+        }
+    }
+
+    private fun initCategoryAutoFill(){
+        // =========================================================================================
+        // ---------------------------- CATEGORY AUTOFILL ------------------------------------------
+        // =========================================================================================
+
+        val initialCategories : MutableList<Category> = mutableListOf()
+        lifecycleScope.launch {
+            initialCategories.addAll(_container.blogRepository.getCategories(1))
+        }
+
+        val categoryAdapter = ArrayAdapter(requireContext(), R.layout.fragment_create_post_list_item, initialCategories)
+        val categorySelectView = binding.categoryCreate.editText as? AutoCompleteTextView
+
+        categorySelectView?.setAdapter(categoryAdapter)
+
+        categorySelectView?.setOnItemClickListener { parent, _, position, _ ->
+            // Handle the selected category
+            selectedCategory = parent.getItemAtPosition(position) as Category
+            binding.categoryCreate.error = null
+            println("USER SELECTED ${selectedCategory!!.name} AS CATEGORY")
+        }
+
+        categorySelectView?.addTextChangedListener {
+            val query = it.toString().trim()
+            if (query.isNotBlank()) {
+                binding.categoryCreate.error = null
+                // Fetch categories based on the search query
+                lifecycleScope.launch {
+                    try {
+                        val searchObject = CategorySearchBody(Category(name=query))
+                        val categories = _container.blogRepository.searchCategory(searchObject)
+                        // TODO: We could clear here for scalability, but this is a mini-project :)
+                        categoryAdapter.addAll(categories)
+                        categoryAdapter.notifyDataSetChanged()
+
+                    } catch (e: Exception) {
+                        // Handle API request failure
+                        println(e.message.toString())
+                    }
+                }
+            } else {
+                selectedCategory = null
+            }
+        }
+    }
+
+    private fun initTagAutoFill(){
+        // ========================================================================================
+        // -------------------------------- TAG AUTOFILL ------------------------------------------
+        // ========================================================================================
+
+        val initialTags : MutableList<Tag> = mutableListOf()
+        lifecycleScope.launch {
+            initialTags.addAll(_container.blogRepository.getTags(1))
+        }
+
+        val tagAdapter = ArrayAdapter(requireContext(), R.layout.fragment_create_post_list_item, initialTags)
+
+        val tagSelectView = (binding.tagCreate.editText as? AutoCompleteTextView)
+        tagSelectView?.setAdapter(tagAdapter)
+
+        tagSelectView?.setOnItemClickListener { parent, _, position, _ ->
+            val selectedTag = parent.getItemAtPosition(position) as Tag
+            if(selectedTags[selectedTag.name] != null){
+                binding.tagCreate.error = "Tag already selected"
+            } else {
+                binding.tagCreate.error = null
+                addChip(selectedTag)
+            }
+            tagSelectView.setText("")
+            println("Selected tags: ${selectedTags.keys.toList()}")
+        }
+
+        tagSelectView?.addTextChangedListener {
+            val query = it.toString().trim()
+
+            if (query.isNotBlank() && query != "") {
+                binding.tagCreate.error = null
+                // Fetch tags based on the search query
+                lifecycleScope.launch {
+                    try {
+                        val searchObject = TagSearchBody(Tag(name=query))
+                        val tags = _container.blogRepository.searchTag(searchObject)
+                        // TODO: We could clear here for scalability, but this is a mini-project :)
+
+                        tagAdapter.addAll(tags)
+                        tagAdapter.notifyDataSetChanged()
+
+                    } catch (e: Exception) {
+                        // Handle API request failure
+                        println(e.message.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun submitPost(){
+        val title = binding.titleCreate.editText?.text.toString()
+        val content = binding.contentCreate.editText?.text.toString()
+        val media = binding.mediaCreate.editText?.text.toString()
+
+        val tagIDs : MutableList<String> = mutableListOf()
+
+        for(tag in selectedTags){
+            tagIDs.add(tag.value)
+        }
+
+        if(title != "" && content != "" && media != "" && tagIDs.isNotEmpty() && selectedCategory != null) {
+            val post = SubmittablePost(
+                title, content, selectedCategory!!.id, tagIDs,media
+            )
+
+            println("POST SUBMITTED!")
+            println(post)
+
+            lifecycleScope.launch {
+                _container.blogRepository.createPost(userSession.accessToken!!,post)
+            }
+        } else {
+            if (title == "") {
+                binding.titleCreate.error = "Please enter a title"
+            } else {
+                binding.titleCreate.error = null
+            }
+            //--
+            if (content == "") {
+                binding.contentCreate.error = "Please enter content"
+            } else {
+                binding.contentCreate.error = null
+            }
+            //--
+            if (media == "") {
+                binding.mediaCreate.error = "Please enter media"
+            } else {
+                binding.mediaCreate.error = null
+            }
+            //--
+            if (tagIDs.isEmpty()) {
+                binding.tagCreate.error = "Please add at least one tag"
+            } else {
+                binding.tagCreate.error = null
+            }
+            //--
+            if (selectedCategory == null) {
+                binding.categoryCreate.error = "Please select a category"
+            } else {
+                binding.categoryCreate.error = null
             }
         }
     }
